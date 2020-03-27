9d70a7add12001ee7e8d9f50497d5915349b5459
Sayam753 <sayamkumar753@yahoo.in>
2020-03-28 04:09:11 +0530

    Added login_required decorator

--- ase/search/views.py
+++ ase/search/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .modules import *
 from .forms import SearchForm
 from blockcypher import get_blockchain_overview, get_address_details, get_block_overview
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -10,47 +12,39 @@ def home(request):
     return render(request, 'search/osint.html')
 
 
+@login_required(redirect_field_name='next', login_url=reverse_lazy('login'))
 def query(request, argument):
     if request.method == "POST":
         form = SearchForm(request.POST)
         if form.is_valid():
-            if request.user.is_authenticated:
-                arg = form.cleaned_data.get('query')
-                response = None
-                if argument == "btc_block_overview":
-                    try:
-                        response = get_block_overview(arg)
-                    except AssertionError:
-                        response = {'error': 'invalid input'}
-                elif argument == "btc_address":
-                    try:
-                        response = get_address_details(arg)
-                    except AssertionError:
-                        response = {'error': 'invalid input'}
-                elif argument == "domain":
-                    response = get_company_detail(arg)
-                elif argument == "email":
-                    response = fetch_email(arg)
-                elif argument == "device":
-                    response = get_device(arg)
-                elif argument == "ip":
-                    response = ip_details(arg)
+            arg = form.cleaned_data.get('query')
+            response = None
+            if argument == "btc_block_overview":
+                try:
+                    response = get_block_overview(arg)
+                except AssertionError:
+                    response = {'error': 'invalid input'}
+            elif argument == "btc_address":
+                try:
+                    response = get_address_details(arg)
+                except AssertionError:
+                    response = {'error': 'invalid input'}
+            elif argument == "domain":
+                response = get_company_detail(arg)
+            elif argument == "email":
+                response = fetch_email(arg)
+            elif argument == "device":
+                response = get_device(arg)
+            elif argument == "ip":
+                response = ip_details(arg)
 
 
-                return render(request, 'search/random.html', {'response': response})
-
-            else:
-                return redirect('login')
-
+            return render(request, 'search/random.html', {'response': response})
 
     else:
-        if request.user.is_authenticated:
-            if argument == "btc_block":
-                response = get_blockchain_overview()
-                return render(request, 'search/random.html', {'response': response})
-            form = SearchForm()
-
-        else:
-            return redirect('login')
+        if argument == "btc_block":
+            response = get_blockchain_overview()
+            return render(request, 'search/random.html', {'response': response})
+        form = SearchForm()
 
     return render(request, 'search/osint.html', {'form': form})
