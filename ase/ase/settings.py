16f9aec321d59d4132b1edb999fc85456bf9f0c2
Nethra16 <nethrag16@gmail.com>
2020-03-28 01:32:20 +0530

    password reset done

--- ase/ase/settings.py
+++ ase/ase/settings.py
@@ -120,6 +120,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+# SESSION_COOKIE_AGE = 3000
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
@@ -132,3 +133,12 @@ AUTH_USER_MODEL = 'users.user'
 #REDIRECTING ROUTE POST LOGIN AND LOGOUT
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='home'
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'projectonosint1@gmail.com'
+EMAIL_HOST_PASSWORD = 'timepass123'
