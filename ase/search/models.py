5cb362e9ce56f55485b06706351d5fa39b8037bb
isha1610200 <44157249+isha1610200@users.noreply.github.com>
2020-07-18 01:19:13 +0530

    added docstring

--- ase/search/models.py
+++ ase/search/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 # Create your models here.
 class Search(models.Model):
+    """
+	-- Summary --
+	Search model has query details.
+	#Field Description
+	> query is a character filed with max possible length = 100, which is basically the user query.
+
+	"""
     query = models.CharField(max_length=100)
 
     def __repr__(self):
